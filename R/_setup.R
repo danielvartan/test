1af4625f9253df6f2596e43006b0f5744cf7a2c1
Daniel Vartanian <danvartan@gmail.com>
2024-12-10 12:35:08 -0300

    Remove Zotero dependence

--- R/_setup.R
+++ R/_setup.R
@@ -69,18 +69,3 @@ thematic::thematic_set_theme(
       legend.frame = ggplot2::element_blank()
     )
 )
-
-# Run `rbbt` -----
-
-# (2024-08-25)
-# This function should work with any version of BetterBibTeX (BBT) for Zotero.
-# Verify if @wmoldham PR was merged in the `rbbt` package (see issue #47
-# <https://github.com/paleolimbot/rbbt/issues/47>). If not, install `rbbt`
-# from @wmoldham fork `remotes::install_github("wmoldham/rbbt", force = TRUE)`.
-
-rutils:::bbt_write_quarto_bib(
-  bib_file = here::here("references.bib"),
-  dir = c("."),
-  pattern = "\\.qmd$",
-  wd = here::here()
-)
