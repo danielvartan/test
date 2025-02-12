06f7d95a0fa24c30f4e21ffc61af14c111fcfd6b
Daniel Vartanian <danvartan@gmail.com>
2025-02-12 03:00:45 -0300

    Add new CSL style

--- R/_setup.R
+++ R/_setup.R
@@ -72,15 +72,18 @@ thematic::thematic_set_theme(
 
 # Run `rbbt` -----
 
+## Uncheck the option "Apply title-casing to titles" in Zotero Better BibTeX
+## preferences (Edit > Settings > Better BibTeX > Miscellaneous).
+
 # (2024-08-25)
 # This function should work with any version of BetterBibTeX (BBT) for Zotero.
 # Verify if @wmoldham PR was merged in the `rbbt` package (see issue #47
 # <https://github.com/paleolimbot/rbbt/issues/47>). If not, install `rbbt`
 # from @wmoldham fork `remotes::install_github("wmoldham/rbbt", force = TRUE)`.
 
-# rutils:::bbt_write_quarto_bib(
-#   bib_file = here::here("references.bib"),
-#   dir = c(".", "qmd"),
-#   pattern = "\\.qmd$",
-#   wd = here::here()
-# )
+rutils:::bbt_write_quarto_bib(
+  bib_file = here::here("references.bib"),
+  dir = c("."),
+  pattern = "\\.qmd$",
+  wd = here::here()
+)
