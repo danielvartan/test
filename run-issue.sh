e588b3fb4b3c605980a123d9cbebda4561d37379
Daniel Vartanian <danielvartan@proton.me>
2026-02-23 23:45:20 -0300

    Add netlogo-2

---
+++ run-issue.sh
@@ -0,0 +1,5 @@
+NetLogo \
+--headless \
+--model "nlogox/test.nlogox" \
+--table "csv/test-issue.csv" \
+--setup-file "xml/test-issue.xml"
---
+++ run-normal.sh
@@ -0,0 +1,5 @@
+NetLogo \
+--headless \
+--model "nlogox/test.nlogox" \
+--table "csv/test-normal.csv" \
+--setup-file "xml/test-normal.xml"
